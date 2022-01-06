3f3c508f19c419710d48bce1bb112ac36b40d6fe
BENEVANIO <BENEVANIO@BENEVANIO>
2022-01-06 10:01:05 -0300

    Adicionar arquivos de projeto.

---
+++ lacoFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos numeros inteiros voce ira digitar:");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Valor : #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma = soma + valor;
+            }
+            Console.WriteLine("soma =" + soma);
+        }
+    }
+}
